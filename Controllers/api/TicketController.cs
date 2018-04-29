ab029d859557f0a566ba75b89c60844be6857850
zqooqz0986 <zqooqz0986@gmail.com>
2018-04-29 21:32:08 +0800

    first commit

---
+++ Controllers/api/TicketController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VueJS.Controllers
+{
+    [Route("api/[controller]")]
+    public class TicketController : Controller
+    {   
+        // POST api/values
+        [HttpPost]
+        public void Buy([FromBody]string value)
+        {
+            
+            using(var client = new HttpClient())
+            {
+
+
+            }
+        }
+
+    }
+}
